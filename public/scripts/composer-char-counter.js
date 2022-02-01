d6f9e7a2a8eee382a7543c094ff74f12e115493a
Jimmy Chuk <jimmychuk@outlook.com>
2022-02-01 14:31:24 -0500

    add backToTop button functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -15,5 +15,36 @@ $(document).ready(function() {
     charCountDisplay.val(charCount)
     
   })
+
+
+
+  //scroll to top and display form and focuses on the textarea
+  $("#backToTop").on("click", function(){
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    const form = $(".new-tweet");
+    const textArea = $("#tweet-text");
+    form.slideDown("slow", () => {
+      textArea.focus();       
+    });
+  })
+
+
+
+  //toggles backToTopButton visibility on scroll
+  $(window).scroll(function(){
+    // console.log(typeof window.scrollY)
+    const backToTopBtn =  $("#backToTop");
+
+    if(window.scrollY === 0){
+      backToTopBtn.hide();
+    }
+
+    if(window.scrollY > 0 && !backToTopBtn.is(":visible")){
+      backToTopBtn.show();
+    }
+
+  })
+ 
 });
 
